71537ba07e8f301c88e42482f79a6e8b347b873e
Carlos <carlosdepet@gmail.com>
2025-03-21 12:08:58 +0100

    Programas/Iphone/navbar

---
+++ Proyectos/apple-proyect/src/components/Highlights.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Highlights = () => {
+  return (
+    <div>
+      HighLights
+    </div>
+  )
+}
+
+export default Highlights
---
+++ Proyectos/apple-proyect/src/main.jsx
@@ -0,0 +1,14 @@
+// @ts-nocheck
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import React from 'react'
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
