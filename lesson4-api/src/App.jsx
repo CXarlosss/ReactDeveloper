accb04e9261b8f8c06e99640ae205352befa1ba5
Carlos <carlosdepet@gmail.com>
2025-03-24 20:50:42 +0100

    ProductList

--- lesson4-api/src/App.jsx
+++ lesson4-api/src/App.jsx
@@ -1,5 +1,5 @@
-
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import React from 'react'
 import Home from './components/Home'
 import ProductDetail from './components/ProductDetail'
@@ -9,20 +9,21 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 
 function App() {
-
   return (
     <>
-    <div className='newClass'>
-      <Header/>
-      <Home/>
-      <Footer/>
-      <Contact/>
-      <ProductDetail/>
-      <ProductList/>
-    </div>
+      <div className='App'>
+        <header>Header</header>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<ProductList />} />
+          <Route path='/products/:id' element={<ProductDetail />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+        <footer>Footer</footer>
+      </div>
     </>
-    
   )
 }
+
 
 export default App
--- lesson4-api/src/main.jsx
+++ lesson4-api/src/main.jsx
@@ -1,12 +1,15 @@
 // @ts-nocheck
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import React from 'react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
