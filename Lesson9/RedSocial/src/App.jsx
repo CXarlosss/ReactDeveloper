b1f9c2313a7fca51e8b453edbba22a2f7ea86a7d
Carlos <carlosdepet@gmail.com>
2025-04-09 12:56:13 +0200

    RedSocial

---
+++ Lesson9/RedSocial/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import React from 'react'
+import Header from './components/layout/general/Header.jsx'
+function App() {
+
+  return (
+  
+    <div className="layout">
+        <Header />
+
+    </div>
+  )
+}
+
+export default App
