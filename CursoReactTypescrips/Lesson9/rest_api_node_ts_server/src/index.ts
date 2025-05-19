e19f3b8afd124558832b90e451c55fb93bee6d7f
Carlos <carlosdepet@gmail.com>
2025-05-19 17:02:23 +0200

    feat: update tsx package to version 4.19.4 with various enhancements and new features
    - Added new files for tsx registration and REPL functionality in both CommonJS and ESM formats.
    - Implemented suppress warnings functionality to filter specific warning messages.
    - Introduced temporary directory management for tsx operations.
    - Enhanced type definitions with RequiredProperty utility type.
    - Updated package.json with new dependencies and scripts for semantic release.
    - Added undefsafe package for safe property access in objects, including examples and tests.
    - Configured TypeScript with strict settings and module resolution for Node.js.

--- CursoReactTypescrips/Lesson9/rest_api_node_ts_server/src/index.ts
+++ CursoReactTypescrips/Lesson9/rest_api_node_ts_server/src/index.ts
@@ -1,3 +1,9 @@
-import { sumar } from './server';
+import { sumar } from './server.js';
 
 console.log('desde index.ts ' + sumar(1, 2));
+
+sumar(1, 2);
+console.log(sumar(1,2))
+sumar(3,5);
+console.log(sumar(3,5))
+console.log('index');
