a7cdcdc773f089a37f83ba70b351af92bf361685
Carlos <carlosdepet@gmail.com>
2025-05-24 14:12:59 +0200

    Types

--- CursoReactTypescrips/Lesson10/OP-Task/Backend/src/models/Project.ts
+++ CursoReactTypescrips/Lesson10/OP-Task/Backend/src/models/Project.ts
@@ -1,29 +1,35 @@
 // src/models/Project.ts
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, PopulatedDoc } from "mongoose";
+import { ITask } from "./Task.js";
 
-export type ProjectType = Document & { // <-- ¡Aquí está el cambio! Ahora se llama ProjectType
-  projectName: string;
-  clientName: string;
-  description: string;
+export type ProjectType = Document & {
+  // <-- ¡Aquí está el cambio! Ahora se llama ProjectType
+  projectName: string;
+  clientName: string;
+  description: string;
+  tasks: PopulatedDoc<ITask & Document>[]; // Opcional, si quieres relacionar tareas con el proyecto
 };
 
-const projectSchema = new Schema<ProjectType>({
+const projectSchema = new Schema<ProjectType>(
+  {
     projectName: {
-        type: String,
-        required: true,
-        trim: true, // Elimina espacios en blanco
+      type: String,
+      required: true,
+      trim: true, // Elimina espacios en blanco
     },
     clientName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-}, {
-    timestamps: true 
-});
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Project = mongoose.model<ProjectType>("Project", projectSchema);
 export default Project;
---
+++ CursoReactTypescrips/Lesson10/OP-Task/Backend/src/models/Task.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+export interface ITask extends Document {
+  name: string;
+  description: string;
+  project: Types.ObjectId;
+  tasks: Types.ObjectId[];
+}
+const TaskSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    project: {
+      type: Types.ObjectId,
+      ref: "Project", // Referencia al modelo Project
+      required: true,
+    },
+    tasks: [
+      {
+        type: Types.ObjectId,
+        ref: "Task",
+      },
+    ],
+  }
+);
+const Task = mongoose.model<ITask>("Task", TaskSchema);
+export default Task;
+
