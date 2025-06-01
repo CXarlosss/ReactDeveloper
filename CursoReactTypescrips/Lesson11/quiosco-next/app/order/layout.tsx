bdbc2023be2bbe46768b28c287d4cbaefc3e9359
Carlos <carlosdepet@gmail.com>
2025-06-01 16:20:51 +0200

    refactor: clean up global styles and fix layout class name; remove unused CSS variables and improve structure

--- CursoReactTypescrips/Lesson11/quiosco-next/app/order/layout.tsx
+++ CursoReactTypescrips/Lesson11/quiosco-next/app/order/layout.tsx
@@ -2,7 +2,7 @@ import Sidebar from "@/components/order/OrderSidebar";
 import OrderSummary from "@/components/order/OrderSummary";
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   return(
-    <div className="md.flex">
+    <div className="md:flex">
         <Sidebar />
         <main className="md:flex-1 md:h-screen md:overflow-y-scroll p-5">
             {children}
