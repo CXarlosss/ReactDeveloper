07bf3d887c7c03d7d7f0f094d5a6e85c8a830fe0
Carlos <carlosdepet@gmail.com>
2025-06-03 16:07:44 +0200

    feat: calcular y mostrar el total del pedido en OrderSummary

--- CursoReactTypescrips/Lesson11/quiosco-next/components/order/OrderSummary.tsx
+++ CursoReactTypescrips/Lesson11/quiosco-next/components/order/OrderSummary.tsx
@@ -2,11 +2,15 @@
 
 import { useStore } from "@/src/store";
 import ProductDetails from "@/components/order/ProductDetails";
-
+import { useMemo } from "react";
+import { formatCurrency } from "@/src/utils";
 
 export default function OrderSummary() {
   const order = useStore((state) => state.order);
-
+  const total = useMemo(
+    () => order.reduce((total, item) => total + item.price * item.quantity, 0),
+    [order]
+  );
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5 bg-gray-50 shadow-inner">
       <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
@@ -20,6 +24,14 @@ export default function OrderSummary() {
           ))}
         </div>
       )}
+      <div className="mt-10 p-5 bg-indigo-100 rounded-xl shadow-md text-center">
+        <p className="text-xl font-semibold text-indigo-700 mb-2">
+          Total a pagar:
+        </p>
+        <p className="text-3xl font-extrabold text-indigo-900">
+          {formatCurrency(total)}
+        </p>
+      </div>
     </aside>
   );
 }
