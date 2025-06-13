ef124e8e5e9d322331b567efd644d5d067c4d37c
Carlos <carlosdepet@gmail.com>
2025-06-13 19:08:55 +0200

    Modificacion de page.tsx dentro de Orders

--- CursoReactTypescrips/Lesson11/quiosco-next/app/admin/orders/page.tsx
+++ CursoReactTypescrips/Lesson11/quiosco-next/app/admin/orders/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
   return (
-    <div>
-      Orders
+    <div className="p-10 bg-slate-400 ">
+      <div className="p-10 bg-slate-400 hover:bg-slate-500 ">
+        <h1 className="text-3xl font-black">Administrar Pedidos</h1>
+      </div>
     </div>
-  )
+  );
 }
